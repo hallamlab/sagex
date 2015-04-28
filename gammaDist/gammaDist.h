1abc7a9cd7b47057ffaed7e29b8608c0bcd913c3
Connor Morgan-Lang <c.morganlang@gmail.com>
2015-04-27 17:59:07 -0700

    Removing redundant directories

--- gammaDist/gammaDist.h
+++
@@ -1,9 +0,0 @@
-
-#include <math.h> 
-
-// Dyadic inversion for gamma distirbution 
-// alpha : distn param 1 
-// beta : distn param 2 
-// p : desired cumulative probability 
-void dyadicInvGam ( double *beta , double *alpha , double *p , double *eps , int *maxIter , double *out ) ; 
-
